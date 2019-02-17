8f8e25d8bb85293ef4daeab38a39cf65a26a7723
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-02-17 11:55:47 +0900

    Implement helloJib method

--- src/main/java/com/demo/project/springbootjibdemo/SpringbootJibDemoApplication.java
+++ src/main/java/com/demo/project/springbootjibdemo/SpringbootJibDemoApplication.java
@@ -2,12 +2,20 @@ package com.demo.project.springbootjibdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootJibDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootJibDemoApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String helloJib(){
+        return "Hello, Jib";
     }
 
 }
