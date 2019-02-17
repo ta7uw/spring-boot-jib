7700792eb2db60a5b857db82a16b048c85b8ed6d
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-02-17 11:25:04 +0900

    Initial commit

---
+++ src/main/java/com/demo/project/springbootjibdemo/SpringbootJibDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.project.springbootjibdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJibDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJibDemoApplication.class, args);
+    }
+
+}
